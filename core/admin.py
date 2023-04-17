f2ffc1da0d55605513545d3e6a1b10235f33c1c6
Andrew <aat-78@mail.ru>
2023-04-17 16:06:56 +0300

    Настройка полей.

--- core/admin.py
+++ core/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 
 from core.models import User
 
-admin.site.register(User)
+
+class UserAdmin(admin.ModelAdmin):
+    exclude = ("password",)
+    readonly_fields = ("date_joined", "last_login")
+    search_fields = ('email', 'first_name', 'last_name', 'username')
+    list_display = ('username', 'email', 'first_name', 'last_name')
+    list_filter = ('is_staff', 'is_active', 'is_superuser')
+
+
+admin.site.register(User, UserAdmin)
+
